eff995b68caeb2c38389daaf647eb6a5c5af4cdf
ty314 <57778727+ty314@users.noreply.github.com>
2019-11-15 11:03:25 +0800

    Update TLLink.h

--- TLLink.h
+++ TLLink.h
@@ -82,6 +82,73 @@ typedef struct
 	TLLNKFSMStatus FSM;
 }TLLink_t;
 
+/**
+ * @brief  Constructor for TLLink.
+ *         Memory allocated by malloc(), note configuration of Heap Size. 
+ * @sys    Local system   
+ * @dev    Local device
+ * @return Reference to a new TLLink
+ */
+TLLink_t *  
+newTLLink(uint8_t sys, uint8_t dev);
+
+/**
+ * @brief Destructor for TLLink.
+ *        Use free() to free memory allocated in newTLLink().
+ * @lnk   Pointer to a reference to TLLink
+ */
+void  
+deleteTLLink(TLLink_t **lnk);
+
+/**
+ * @brief  Constructor for TLLNKPacket.
+ *         Memory allocated by malloc(), note configuration of Heap Size. 
+ * @size   Max size of value
+ * @return Reference to a new TLLNKPacket
+ */
+TLLNKPacket_t *  
+newTLLNKPacket(uint8_t size);
+
+/**
+ * @brief Destructor for TLLNKPacket.
+ *        Use free() to free memory allocated in newTLLNKPacket().
+ * @pkt   Pointer to a reference to TLLNKPacket
+ */
+void  
+deleteTLLNKPacket(TLLNKPacket_t **pkt);
+
+/**
+ * @brief  Pack sent packet.
+ *         Assume message (Tag, Len, Val) already set.
+ * @pkt    Sent packet to pack
+ * @lnk    TLLink 
+ * @calc   Calc Checksum if non-zero, else fill Checksum with Tag and Len.
+ * @return Packet size, limit to 0xFF.
+ */
+uint8_t 
+TLLNKPack(TLLNKPacket_t *pkt, TLLink_t *lnk, uint8_t calc);
+
+/**
+ * @brief  Parse one byte with state machine according to packet protocol.
+ *         Only satisfy completeness, not correctness.
+ *         User should then call TLLNKVerify() to verify Checksum.
+ * @c      Byte caller supplied for parsing
+ * @pkt    Packet parsed into
+ * @lnk    TLLink
+ * @return 0 if complete packet parsed, else 1.
+ */
+uint8_t 
+TLLNKParse(uint8_t c, TLLNKPacket_t *pkt, TLLink_t *lnk);
+
+/**
+ * @brief  Verify Checksum and update IOTLink after complete packet parsed
+ * @pkt    Packet has been parsed to verify
+ * @lnk    TLLink
+ * @return 1 if correct, else 0.
+ */
+uint8_t
+TLLNKVerify(TLLNKPacket_t *pkt, TLLink_t *lnk);
+
 #ifdef __cplusplus
   }
 #endif
