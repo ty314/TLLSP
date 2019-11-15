7ee2b6929b776ea2940a3e69cf702ccc93ca2f72
ty314 <57778727+ty314@users.noreply.github.com>
2019-11-15 12:45:04 +0800

    Update TLSLink.h

--- TLSLink.h
+++ TLSLink.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2018, LL Tang
  * All rights reserved
  *
- * @file    TLLink.h
+ * @file    TLSLink.h
  * @author  LL.T
  * @version 1.0.0
  * @date    2018/05/28
@@ -10,13 +10,13 @@
  */
  
 /* Define to prevent recursive inclusion */
-#ifndef __TLLINK_H_
-#define __TLLINK_H_ 
+#ifndef __TLSLINK_H_
+#define __TLSLINK_H_ 
 
 /**
- * @brief TLLink version, format is x.y.z
+ * @brief TLSLink version, format is x.y.z
  */
-#define TLLNK_VERSION 100
+#define TLSLNK_VERSION 100
 
 #ifdef __cplusplus
   extern "C" {
@@ -28,26 +28,26 @@
 /**
  * @brief Macros of packet protocol
  */		
-#define TLLNK_MARK 0x7E  // Packet head mark
-#define TLLNK_VERIFY_OFFSET 1  // Byte calc Checksum from
-#define TLLNK_HEADS_VERIFY  6  // Calc + Seq + Sys + Dev + Tag + Len  
-#define TLLNK_MIN_SIZE      9  // Size no value included
+#define TLSLNK_MARK 0x7E  // Packet head mark
+#define TLSLNK_VERIFY_OFFSET 1  // Byte calc Checksum from
+#define TLSLNK_HEADS_VERIFY  6  // Calc + Seq + Sys + Dev + Tag + Len  
+#define TLSLNK_MIN_SIZE      9  // Size no value included
 
 /**
  * @brief Based on FSM, decompose parse of packet into several stable status.
  */		
 typedef enum {
-	TLLNK_FSM_MARK = 0,
-	TLLNK_FSM_CALC,
-	TLLNK_FSM_SEQ,
-	TLLNK_FSM_SYS,
-	TLLNK_FSM_DEV,
-	TLLNK_FSM_TAG,
-	TLLNK_FSM_LEN,
-	TLLNK_FSM_VAL,
-	TLLNK_FSM_CKL,
-	TLLNK_FSM_CKH
-}TLLNKFSMStatus;
+	TLSLNK_FSM_MARK = 0,
+	TLSLNK_FSM_CALC,
+	TLSLNK_FSM_SEQ,
+	TLSLNK_FSM_SYS,
+	TLSLNK_FSM_DEV,
+	TLSLNK_FSM_TAG,
+	TLSLNK_FSM_LEN,
+	TLSLNK_FSM_VAL,
+	TLSLNK_FSM_CKL,
+	TLSLNK_FSM_CKH
+}TLSLNKFSMStatus;
 
 /**
  * @brief Packet protocol
@@ -63,11 +63,11 @@ typedef struct
 	uint8_t Len;     // Length
 	uint8_t *ValPtr; // Value
 	uint16_t Checksum;
-}TLLNKPacket_t;
+}TLSLNKPacket_t;
 
 /**
- * @brief Data structure of TLLink.
- *        It doesn't contain instance of TLLNKPacket_t, whose use is up to implementer.
+ * @brief Data structure of TLSLink.
+ *        It doesn't contain instance of TLSLNKPacket_t, whose use is up to implementer.
  *        Implementer can use one for sent and received, or two respectively.
  */	
 typedef struct 
@@ -79,78 +79,78 @@ typedef struct
 	uint32_t NbrErr;    // Number of error packets
 	uint32_t NbrValid;  // Number of valid packets
 	uint8_t Cnt;        // Counter of length has got
-	TLLNKFSMStatus FSM;
-}TLLink_t;
+	TLSLNKFSMStatus FSM;
+}TLSLink_t;
 
 /**
- * @brief  Constructor for TLLink.
+ * @brief  Constructor for TLSLink.
  *         Memory allocated by malloc(), note configuration of Heap Size. 
  * @sys    Local system   
  * @dev    Local device
- * @return Reference to a new TLLink
+ * @return Reference to a new TLSLink
  */
-TLLink_t *  
-newTLLink(uint8_t sys, uint8_t dev);
+TLSLink_t *  
+newTLSLink(uint8_t sys, uint8_t dev);
 
 /**
- * @brief Destructor for TLLink.
- *        Use free() to free memory allocated in newTLLink().
- * @lnk   Pointer to a reference to TLLink
+ * @brief Destructor for TLSLink.
+ *        Use free() to free memory allocated in newTLSLink().
+ * @lnk   Pointer to a reference to TLSLink
  */
 void  
-deleteTLLink(TLLink_t **lnk);
+deleteTLSLink(TLSLink_t **lnk);
 
 /**
- * @brief  Constructor for TLLNKPacket.
+ * @brief  Constructor for TLSLNKPacket.
  *         Memory allocated by malloc(), note configuration of Heap Size. 
  * @size   Max size of value
- * @return Reference to a new TLLNKPacket
+ * @return Reference to a new TLSLNKPacket
  */
-TLLNKPacket_t *  
-newTLLNKPacket(uint8_t size);
+TLSLNKPacket_t *  
+newTLSLNKPacket(uint8_t size);
 
 /**
- * @brief Destructor for TLLNKPacket.
- *        Use free() to free memory allocated in newTLLNKPacket().
- * @pkt   Pointer to a reference to TLLNKPacket
+ * @brief Destructor for TLSLNKPacket.
+ *        Use free() to free memory allocated in newTLSLNKPacket().
+ * @pkt   Pointer to a reference to TLSLNKPacket
  */
 void  
-deleteTLLNKPacket(TLLNKPacket_t **pkt);
+deleteTLSLNKPacket(TLSLNKPacket_t **pkt);
 
 /**
  * @brief  Pack sent packet.
  *         Assume message (Tag, Len, Val) already set.
  * @pkt    Sent packet to pack
- * @lnk    TLLink 
+ * @lnk    TLSLink 
  * @calc   Calc Checksum if non-zero, else fill Checksum with Tag and Len.
  * @return Packet size, limit to 0xFF.
  */
 uint8_t 
-TLLNKPack(TLLNKPacket_t *pkt, TLLink_t *lnk, uint8_t calc);
+TLSLNKPack(TLSLNKPacket_t *pkt, TLSLink_t *lnk, uint8_t calc);
 
 /**
  * @brief  Parse one byte with state machine according to packet protocol.
  *         Only satisfy completeness, not correctness.
- *         User should then call TLLNKVerify() to verify Checksum.
+ *         User should then call TLSLNKVerify() to verify Checksum.
  * @c      Byte caller supplied for parsing
  * @pkt    Packet parsed into
- * @lnk    TLLink
+ * @lnk    TLSLink
  * @return 0 if complete packet parsed, else 1.
  */
 uint8_t 
-TLLNKParse(uint8_t c, TLLNKPacket_t *pkt, TLLink_t *lnk);
+TLSLNKParse(uint8_t c, TLSLNKPacket_t *pkt, TLSLink_t *lnk);
 
 /**
- * @brief  Verify Checksum and update IOTLink after complete packet parsed
+ * @brief  Verify Checksum and update TLSLink after complete packet parsed
  * @pkt    Packet has been parsed to verify
- * @lnk    TLLink
+ * @lnk    TLSLink
  * @return 1 if correct, else 0.
  */
 uint8_t
-TLLNKVerify(TLLNKPacket_t *pkt, TLLink_t *lnk);
+TLSLNKVerify(TLSLNKPacket_t *pkt, TLSLink_t *lnk);
 
 #ifdef __cplusplus
   }
 #endif
 
-#endif /* __TLLINK_H_ */
+#endif /* __TLSLINK_H_ */
